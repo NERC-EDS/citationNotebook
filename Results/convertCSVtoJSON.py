5a514399e78a0c4c86f33bc64c14e7e1b7e71adf
Matthew Nichols <matnic@ceh.ac.uk>
2023-03-29 15:30:54 +0100

    Function to convert csv results to json organised by dataset publisher, and the json result file

---
+++ Results/convertCSVtoJSON.py
@@ -0,0 +1,19 @@
+import csv
+import json
+
+def convertCSVtoJSON(csv_file_path, json_file_path):
+    
+    data = {}
+
+    # Read the CSV file and group the data by `dataset_publisher_processed`
+    with open(csv_file_path, "r", encoding="utf-8") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            publisher = row["dataset_publisher_processed"]
+            if publisher not in data:
+                data[publisher] = []
+            data[publisher].append(row)
+
+    # Write the data to a JSON file
+    with open(json_file_path, "w", encoding="utf-8") as jsonfile:
+        json.dump(data, jsonfile, indent=4)
