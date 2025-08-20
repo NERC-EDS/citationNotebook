666eb54b5a4490d78434af16c6a8ab12f2351683
PalaeoMatNic <matnic@ceh.ac.uk>
2025-08-20 16:59:35 +0100

    Updated overton functions, still to be tested

---
+++ nerc_citations_2025/citations_fun/getOvertonCitations.py
@@ -0,0 +1,87 @@
+# functions to harvest nerc dataset citations from Overton
+
+import requests, time, json
+import pandas as pd
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
+def overton_api_request(doi):
+    url = "https://app.overton.io/documents.php"
+    params ={
+        "plain_dois_cited": doi,
+        "format": "json",
+        "api_key": "3c7b1a-849d90-77f9da",
+    }
+    r = session.get(url, params=params, timeout=30)
+    print(r.status_code, "DOI: ", doi)
+    r.raise_for_status()
+    return r.json().get('results', [])
+
+
+def getOvertonCitations(nerc_datacite_dois_df):
+
+    session = requests.Session()
+    retries = Retry(
+        total=5,
+        backoff_factor=1,
+        status_forcelist=[429, 500, 502, 503, 504],
+        allowed_methods=["GET"]
+    )
+    session.mount("https://", HTTPAdapter(max_retries=retries))
+
+
+    results = []
+
+    for doi in nerc_datacite_dois_df.data_doi:
+        start = time.time()
+
+        try:
+            data = overton_api_request(doi)
+            if data:
+                results.append(data)
+        except requests.RequestException as e:
+            print(f"Error on {doi}: {e}")
+            continue
+
+        elapsed = time.time() - start
+        # enforce at least 1s between requests
+        if elapsed < 1:
+            time.sleep(1.05 - elapsed)
+        
+    
+    return results
+
+def processOvertonResults(results):
+    flat_results = [d for sublist in results for d in sublist]
+    overton_df = pd.DataFrame(flat_results)
+    # overton_df.to_csv("results/overton_list.csv", index= False )
+
+    # extract columns we're interested in
+    overton_df_sub = overton_df[['title', 'authors', 'published_on', 'document_url', 'highlights']]
+
+
+    # extract relation type and data doi from highlights column dictionaries
+    extracted = overton_df_sub["highlights"].apply(
+        lambda x: {
+            "type": x[0]["type"] if isinstance(x, list) and len(x) > 0 else None,
+            "doi": x[0]["doi"] if isinstance(x, list) and len(x) > 0 else None,
+        }
+    )
+
+    # Turn the Series of dicts into a DataFrame and join to the original
+    extracted_df = pd.DataFrame(extracted.tolist())
+    overton_df_sub = overton_df_sub.join(extracted_df)
+
+    # drop highlights column - no longer needed
+    overton_df_sub = overton_df_sub.drop('highlights', axis = 1)
+
+    # rename columns
+    cols = {"title":"pub_title", 'authors':'pub_authors', 'published_on':'pub_date', 'document_url':'pub_doi', 'type':'relation_type', 'doi':'data_doi'}
+    overton_df_sub = overton_df_sub.rename(columns = cols)
+
+    # write to file
+    overton_df_sub.to_csv("results/latest_results_overton.csv", index= False )
+
+    return overton_df_sub
+
